1989bc89ca0b9f8a250a7c6166bc93000bfb3b15
Atma1 <atmasusanto08@gmail.com>
2025-09-05 21:45:33 +0800

    fix: kepulauan bangka belitung image not appearing

--- src/data/provinces.js
+++ src/data/provinces.js
@@ -14,7 +14,7 @@ const JSON_BY_KEY = Object.fromEntries(
     const file = path.split("/").pop();            // "DKIJakarta.json"
     const key = file.replace(/\.json$/i, "");      // "DKIJakarta"
     // Vite expose JSON di mod.default (atau langsung mod pada beberapa versi)
-    return [key, (mod && mod.default) || mod];
+    return [key, mod?.default || mod];
   })
 );
 
@@ -24,6 +24,7 @@ const JSON_BY_KEY = Object.fromEntries(
 const JSON_ALIAS = {
   "Daerah Khusus Jakarta": "DKIJakarta",
   "Daerah Istimewa Yogyakarta": "DIYogyakarta",
+  "Kepulauan Bangka Belitung": "BangkaBelitung"
 };
 
 function jsonKeyFor(id) {
@@ -56,38 +57,49 @@ const DEFAULT_DESC =
    3) DATA DASAR (x, y, culture) — tetap seperti semula
 ======================= */
 const RAW_PROVINCES = [
-  { id: "Aceh", x: 0.06, y: 0.82,
-    culture: [{ t: "Tari", d: "Saman" },{ t: "Kuliner", d: "Mie Aceh" },{ t: "Musik", d: "Rapai" }],
+  {
+    id: "Aceh", x: 0.06, y: 0.82,
+    culture: [{ t: "Tari", d: "Saman" }, { t: "Kuliner", d: "Mie Aceh" }, { t: "Musik", d: "Rapai" }],
   },
-  { id: "Sumatera Utara", x: 0.1, y: 0.75,
-    culture: [{ t: "Tari", d: "Saman" },{ t: "Kuliner", d: "Mie Aceh" },{ t: "Musik", d: "Rapai" }],
+  {
+    id: "Sumatera Utara", x: 0.1, y: 0.75,
+    culture: [{ t: "Tari", d: "Saman" }, { t: "Kuliner", d: "Mie Aceh" }, { t: "Musik", d: "Rapai" }],
   },
-  { id: "Sumatera Barat", x: 0.1, y: 0.64,
-    culture: [{ t: "Tari", d: "Saman" },{ t: "Kuliner", d: "Mie Aceh" },{ t: "Musik", d: "Rapai" }],
+  {
+    id: "Sumatera Barat", x: 0.1, y: 0.64,
+    culture: [{ t: "Tari", d: "Saman" }, { t: "Kuliner", d: "Mie Aceh" }, { t: "Musik", d: "Rapai" }],
   },
-  { id: "Riau", x: 0.15, y: 0.68,
-    culture: [{ t: "Tari", d: "Saman" },{ t: "Kuliner", d: "Mie Aceh" },{ t: "Musik", d: "Rapai" }],
+  {
+    id: "Riau", x: 0.15, y: 0.68,
+    culture: [{ t: "Tari", d: "Saman" }, { t: "Kuliner", d: "Mie Aceh" }, { t: "Musik", d: "Rapai" }],
   },
-  { id: "Kepulauan Riau", x: 0.22, y: 0.75,
-    culture: [{ t: "Tari", d: "Saman" },{ t: "Kuliner", d: "Mie Aceh" },{ t: "Musik", d: "Rapai" }],
+  {
+    id: "Kepulauan Riau", x: 0.22, y: 0.75,
+    culture: [{ t: "Tari", d: "Saman" }, { t: "Kuliner", d: "Mie Aceh" }, { t: "Musik", d: "Rapai" }],
   },
-  { id: "Jambi", x: 0.17, y: 0.62,
-    culture: [{ t: "Tari", d: "Saman" },{ t: "Kuliner", d: "Mie Aceh" },{ t: "Musik", d: "Rapai" }],
+  {
+    id: "Jambi", x: 0.17, y: 0.62,
+    culture: [{ t: "Tari", d: "Saman" }, { t: "Kuliner", d: "Mie Aceh" }, { t: "Musik", d: "Rapai" }],
   },
-  { id: "Sumatera Selatan", x: 0.19, y: 0.57,
-    culture: [{ t: "Tari", d: "Saman" },{ t: "Kuliner", d: "Mie Aceh" },{ t: "Musik", d: "Rapai" }],
+  {
+    id: "Sumatera Selatan", x: 0.19, y: 0.57,
+    culture: [{ t: "Tari", d: "Saman" }, { t: "Kuliner", d: "Mie Aceh" }, { t: "Musik", d: "Rapai" }],
   },
-  { id: "Bengkulu", x: 0.13, y: 0.55,
-    culture: [{ t: "Tari", d: "Saman" },{ t: "Kuliner", d: "Mie Aceh" },{ t: "Musik", d: "Rapai" }],
+  {
+    id: "Bengkulu", x: 0.13, y: 0.55,
+    culture: [{ t: "Tari", d: "Saman" }, { t: "Kuliner", d: "Mie Aceh" }, { t: "Musik", d: "Rapai" }],
   },
-  { id: "Lampung", x: 0.19, y: 0.51,
-    culture: [{ t: "Tari", d: "Saman" },{ t: "Kuliner", d: "Mie Aceh" },{ t: "Musik", d: "Rapai" }],
+  {
+    id: "Lampung", x: 0.19, y: 0.51,
+    culture: [{ t: "Tari", d: "Saman" }, { t: "Kuliner", d: "Mie Aceh" }, { t: "Musik", d: "Rapai" }],
   },
-  { id: "Kepulauan Bangka Belitung", x: 0.24, y: 0.63,
-    culture: [{ t: "Tari", d: "Saman" },{ t: "Kuliner", d: "Mie Aceh" },{ t: "Musik", d: "Rapai" }],
+  {
+    id: "Kepulauan Bangka Belitung", x: 0.24, y: 0.63,
+    culture: [{ t: "Tari", d: "Saman" }, { t: "Kuliner", d: "Mie Aceh" }, { t: "Musik", d: "Rapai" }],
   },
-  { id: "Banten", x: 0.21, y: 0.45,
-    culture: [{ t: "Tari", d: "Saman" },{ t: "Kuliner", d: "Mie Aceh" },{ t: "Musik", d: "Rapai" }],
+  {
+    id: "Banten", x: 0.21, y: 0.45,
+    culture: [{ t: "Tari", d: "Saman" }, { t: "Kuliner", d: "Mie Aceh" }, { t: "Musik", d: "Rapai" }],
   },
   {
     id: "Daerah Khusus Jakarta", x: 0.25, y: 0.5,
@@ -97,82 +109,105 @@ const RAW_PROVINCES = [
       { t: "Musik", d: "Gambang Kromong" },
     ],
   },
-  { id: "Jawa Barat", x: 0.25, y: 0.428,
-    culture: [{ t: "Tari", d: "Saman" },{ t: "Kuliner", d: "Mie Aceh" },{ t: "Musik", d: "Rapai" }],
+  {
+    id: "Jawa Barat", x: 0.25, y: 0.428,
+    culture: [{ t: "Tari", d: "Saman" }, { t: "Kuliner", d: "Mie Aceh" }, { t: "Musik", d: "Rapai" }],
   },
-  { id: "Jawa Tengah", x: 0.3, y: 0.43,
-    culture: [{ t: "Tari", d: "Saman" },{ t: "Kuliner", d: "Mie Aceh" },{ t: "Musik", d: "Rapai" }],
+  {
+    id: "Jawa Tengah", x: 0.3, y: 0.43,
+    culture: [{ t: "Tari", d: "Saman" }, { t: "Kuliner", d: "Mie Aceh" }, { t: "Musik", d: "Rapai" }],
   },
-  { id: "Daerah Istimewa Yogyakarta", x: 0.32, y: 0.39,
-    culture: [{ t: "Tari", d: "Saman" },{ t: "Kuliner", d: "Mie Aceh" },{ t: "Musik", d: "Rapai" }],
+  {
+    id: "Daerah Istimewa Yogyakarta", x: 0.32, y: 0.39,
+    culture: [{ t: "Tari", d: "Saman" }, { t: "Kuliner", d: "Mie Aceh" }, { t: "Musik", d: "Rapai" }],
   },
-  { id: "Jawa Timur", x: 0.35, y: 0.43,
-    culture: [{ t: "Tari", d: "Saman" },{ t: "Kuliner", d: "Mie Aceh" },{ t: "Musik", d: "Rapai" }],
+  {
+    id: "Jawa Timur", x: 0.35, y: 0.43,
+    culture: [{ t: "Tari", d: "Saman" }, { t: "Kuliner", d: "Mie Aceh" }, { t: "Musik", d: "Rapai" }],
   },
   {
     id: "Bali", x: 0.4, y: 0.44,
-    culture: [{ t: "Tari", d: "Kecak" },{ t: "Kuliner", d: "Ayam Betutu" },{ t: "Musik", d: "Gamelan Bali" }],
+    culture: [{ t: "Tari", d: "Kecak" }, { t: "Kuliner", d: "Ayam Betutu" }, { t: "Musik", d: "Gamelan Bali" }],
   },
-  { id: "Nusa Tenggara Barat", x: 0.45, y: 0.44,
-    culture: [{ t: "Tari", d: "Saman" },{ t: "Kuliner", d: "Mie Aceh" },{ t: "Musik", d: "Rapai" }],
+  {
+    id: "Nusa Tenggara Barat", x: 0.45, y: 0.44,
+    culture: [{ t: "Tari", d: "Saman" }, { t: "Kuliner", d: "Mie Aceh" }, { t: "Musik", d: "Rapai" }],
   },
-  { id: "Nusa Tenggara Timur", x: 0.55, y: 0.42,
-    culture: [{ t: "Tari", d: "Saman" },{ t: "Kuliner", d: "Mie Aceh" },{ t: "Musik", d: "Rapai" }],
+  {
+    id: "Nusa Tenggara Timur", x: 0.55, y: 0.42,
+    culture: [{ t: "Tari", d: "Saman" }, { t: "Kuliner", d: "Mie Aceh" }, { t: "Musik", d: "Rapai" }],
   },
-  { id: "Kalimantan Barat", x: 0.29, y: 0.7,
-    culture: [{ t: "Tari", d: "Saman" },{ t: "Kuliner", d: "Mie Aceh" },{ t: "Musik", d: "Rapai" }],
+  {
+    id: "Kalimantan Barat", x: 0.29, y: 0.7,
+    culture: [{ t: "Tari", d: "Saman" }, { t: "Kuliner", d: "Mie Aceh" }, { t: "Musik", d: "Rapai" }],
   },
-  { id: "Kalimantan Tengah", x: 0.37, y: 0.65,
-    culture: [{ t: "Tari", d: "Saman" },{ t: "Kuliner", d: "Mie Aceh" },{ t: "Musik", d: "Rapai" }],
+  {
+    id: "Kalimantan Tengah", x: 0.37, y: 0.65,
+    culture: [{ t: "Tari", d: "Saman" }, { t: "Kuliner", d: "Mie Aceh" }, { t: "Musik", d: "Rapai" }],
   },
-  { id: "Kalimantan Selatan", x: 0.42, y: 0.62,
-    culture: [{ t: "Tari", d: "Saman" },{ t: "Kuliner", d: "Mie Aceh" },{ t: "Musik", d: "Rapai" }],
+  {
+    id: "Kalimantan Selatan", x: 0.42, y: 0.62,
+    culture: [{ t: "Tari", d: "Saman" }, { t: "Kuliner", d: "Mie Aceh" }, { t: "Musik", d: "Rapai" }],
   },
-  { id: "Kalimantan Timur", x: 0.43, y: 0.78,
-    culture: [{ t: "Tari", d: "Saman" },{ t: "Kuliner", d: "Mie Aceh" },{ t: "Musik", d: "Rapai" }],
+  {
+    id: "Kalimantan Timur", x: 0.43, y: 0.78,
+    culture: [{ t: "Tari", d: "Saman" }, { t: "Kuliner", d: "Mie Aceh" }, { t: "Musik", d: "Rapai" }],
   },
-  { id: "Kalimantan Utara", x: 0.41, y: 0.85,
-    culture: [{ t: "Tari", d: "Saman" },{ t: "Kuliner", d: "Mie Aceh" },{ t: "Musik", d: "Rapai" }],
+  {
+    id: "Kalimantan Utara", x: 0.41, y: 0.85,
+    culture: [{ t: "Tari", d: "Saman" }, { t: "Kuliner", d: "Mie Aceh" }, { t: "Musik", d: "Rapai" }],
   },
-  { id: "Sulawesi Utara", x: 0.58, y: 0.82,
-    culture: [{ t: "Tari", d: "Saman" },{ t: "Kuliner", d: "Mie Aceh" },{ t: "Musik", d: "Rapai" }],
+  {
+    id: "Sulawesi Utara", x: 0.58, y: 0.82,
+    culture: [{ t: "Tari", d: "Saman" }, { t: "Kuliner", d: "Mie Aceh" }, { t: "Musik", d: "Rapai" }],
   },
-  { id: "Gorontalo", x: 0.56, y: 0.73,
-    culture: [{ t: "Tari", d: "Saman" },{ t: "Kuliner", d: "Mie Aceh" },{ t: "Musik", d: "Rapai" }],
+  {
+    id: "Gorontalo", x: 0.56, y: 0.73,
+    culture: [{ t: "Tari", d: "Saman" }, { t: "Kuliner", d: "Mie Aceh" }, { t: "Musik", d: "Rapai" }],
   },
-  { id: "Sulawesi Tengah", x: 0.52, y: 0.67,
-    culture: [{ t: "Tari", d: "Saman" },{ t: "Kuliner", d: "Mie Aceh" },{ t: "Musik", d: "Rapai" }],
+  {
+    id: "Sulawesi Tengah", x: 0.52, y: 0.67,
+    culture: [{ t: "Tari", d: "Saman" }, { t: "Kuliner", d: "Mie Aceh" }, { t: "Musik", d: "Rapai" }],
   },
-  { id: "Sulawesi Barat", x: 0.47, y: 0.6,
-    culture: [{ t: "Tari", d: "Saman" },{ t: "Kuliner", d: "Mie Aceh" },{ t: "Musik", d: "Rapai" }],
+  {
+    id: "Sulawesi Barat", x: 0.47, y: 0.6,
+    culture: [{ t: "Tari", d: "Saman" }, { t: "Kuliner", d: "Mie Aceh" }, { t: "Musik", d: "Rapai" }],
   },
-  { id: "Sulawesi Selatan", x: 0.51, y: 0.52,
-    culture: [{ t: "Tari", d: "Saman" },{ t: "Kuliner", d: "Mie Aceh" },{ t: "Musik", d: "Rapai" }],
+  {
+    id: "Sulawesi Selatan", x: 0.51, y: 0.52,
+    culture: [{ t: "Tari", d: "Saman" }, { t: "Kuliner", d: "Mie Aceh" }, { t: "Musik", d: "Rapai" }],
   },
-  { id: "Sulawesi Tenggara", x: 0.55, y: 0.57,
-    culture: [{ t: "Tari", d: "Saman" },{ t: "Kuliner", d: "Mie Aceh" },{ t: "Musik", d: "Rapai" }],
+  {
+    id: "Sulawesi Tenggara", x: 0.55, y: 0.57,
+    culture: [{ t: "Tari", d: "Saman" }, { t: "Kuliner", d: "Mie Aceh" }, { t: "Musik", d: "Rapai" }],
   },
-  { id: "Maluku", x: 0.68, y: 0.53,
-    culture: [{ t: "Tari", d: "Saman" },{ t: "Kuliner", d: "Mie Aceh" },{ t: "Musik", d: "Rapai" }],
+  {
+    id: "Maluku", x: 0.68, y: 0.53,
+    culture: [{ t: "Tari", d: "Saman" }, { t: "Kuliner", d: "Mie Aceh" }, { t: "Musik", d: "Rapai" }],
   },
-  { id: "Maluku Utara", x: 0.65, y: 0.73,
-    culture: [{ t: "Tari", d: "Saman" },{ t: "Kuliner", d: "Mie Aceh" },{ t: "Musik", d: "Rapai" }],
+  {
+    id: "Maluku Utara", x: 0.65, y: 0.73,
+    culture: [{ t: "Tari", d: "Saman" }, { t: "Kuliner", d: "Mie Aceh" }, { t: "Musik", d: "Rapai" }],
   },
   {
     id: "Papua", x: 0.88, y: 0.6,
-    culture: [{ t: "Tari", d: "Tari Perang" },{ t: "Kuliner", d: "Papeda" },{ t: "Musik", d: "Tifa" }],
+    culture: [{ t: "Tari", d: "Tari Perang" }, { t: "Kuliner", d: "Papeda" }, { t: "Musik", d: "Tifa" }],
   },
-  { id: "Papua Barat", x: 0.74, y: 0.65,
-    culture: [{ t: "Tari", d: "Saman" },{ t: "Kuliner", d: "Mie Aceh" },{ t: "Musik", d: "Rapai" }],
+  {
+    id: "Papua Barat", x: 0.74, y: 0.65,
+    culture: [{ t: "Tari", d: "Saman" }, { t: "Kuliner", d: "Mie Aceh" }, { t: "Musik", d: "Rapai" }],
   },
-  { id: "Papua Selatan", x: 0.9, y: 0.45,
-    culture: [{ t: "Tari", d: "Saman" },{ t: "Kuliner", d: "Mie Aceh" },{ t: "Musik", d: "Rapai" }],
+  {
+    id: "Papua Selatan", x: 0.9, y: 0.45,
+    culture: [{ t: "Tari", d: "Saman" }, { t: "Kuliner", d: "Mie Aceh" }, { t: "Musik", d: "Rapai" }],
   },
-  { id: "Papua Tengah", x: 0.84, y: 0.52,
-    culture: [{ t: "Tari", d: "Saman" },{ t: "Kuliner", d: "Mie Aceh" },{ t: "Musik", d: "Rapai" }],
+  {
+    id: "Papua Tengah", x: 0.84, y: 0.52,
+    culture: [{ t: "Tari", d: "Saman" }, { t: "Kuliner", d: "Mie Aceh" }, { t: "Musik", d: "Rapai" }],
   },
-  { id: "Papua Pegunungan", x: 0.93, y: 0.54,
-    culture: [{ t: "Tari", d: "Saman" },{ t: "Kuliner", d: "Mie Aceh" },{ t: "Musik", d: "Rapai" }],
+  {
+    id: "Papua Pegunungan", x: 0.93, y: 0.54,
+    culture: [{ t: "Tari", d: "Saman" }, { t: "Kuliner", d: "Mie Aceh" }, { t: "Musik", d: "Rapai" }],
   },
 ];
 
@@ -227,8 +262,8 @@ function enrichWithJson(baseProv) {
       (Array.isArray(baseProv.images) && baseProv.images.length
         ? baseProv.images
         : galleryFromJson.length
-        ? galleryFromJson
-        : baseProv.images) || [],
+          ? galleryFromJson
+          : baseProv.images) || [],
   };
 }
 
